cf053dd54999a24a10c4fc47e5bcd1a99a401b0d
ireya <rhresh1289@gmail.com>
2024-05-20 23:32:23 +0530

    Initial commit

---
+++ mini_project/code/src/lib/pinecone-client.ts
@@ -0,0 +1,31 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { env } from "./config";
+import { delay } from "./utils";
+
+let pineconeClientInstance: Pinecone | null = null;
+
+// Initialize index and ready to be accessed.
+async function initPineconeClient() {
+  try {
+    const pineconeClient = new Pinecone({
+      apiKey: env.PINECONE_API_KEY,
+      // environment: env.PINECONE_ENVIRONMENT,
+      });
+    const indexName = env.PINECONE_INDEX_NAME;
+
+    const existingIndexes = await pineconeClient.listIndexes();
+
+    return pineconeClient;
+  } catch (error) {
+    console.error("error", error);
+    throw new Error("Failed to initialize Pinecone Client");
+  }
+}
+
+export async function getPineconeClient() {
+  if (!pineconeClientInstance) {
+    pineconeClientInstance = await initPineconeClient();
+  }
+
+  return pineconeClientInstance;
+}
